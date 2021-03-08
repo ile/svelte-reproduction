1cd9e337b0f5a446c73e6618f1ef299aacbea98c
Ilkka Huotari <ilkkah@gmail.com>
2021-03-08 11:01:57 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<svelte:options tag="hello-svelte" />
+
+<script>
+	function click(e) {
+		e.preventDefault();
+		history.pushState({}, 'Home', '/');
+		document.dispatchEvent(new Event('url-changed'));
+	}
+</script>
+
+<main>
+	<p>Visit the <a href="/" on:click={click}>root</a>. </p>
+</main>
+
